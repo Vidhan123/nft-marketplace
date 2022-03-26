c6ae839ff5aa85d1d256430a9e244c62b024f497
varunmehta14 <mehtavarunj@gmail.com>
2022-03-27 03:03:00 +0530

    create

--- frontend/src/pages/CreateUser.js
+++ frontend/src/pages/CreateUser.js
@@ -16,7 +16,7 @@ const styles = makeStyles(() => ({
     },
 }))
 
-const CreateUser = (createUserFromApp) => {
+const CreateUser = ({createUserFromApp}) => {
     const classes = styles()
     const [src, setSrc] = React.useState("https://ipfs.infura.io/ipfs/QmZ7smTQUxBXZW7Bx14VuxPgBurp2PcF7H9G6F74nC9viX");
     const [clickedChange, setClickedChange] = React.useState(false);
